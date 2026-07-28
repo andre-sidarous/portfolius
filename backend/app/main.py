caa1d3834c27a723305270331b21abf1deb175dd
Andre Sidarous <andi.sidarous@gmail.com>
2026-07-28 00:08:07 -0400

    setup nextjs for frontend and fastapi for backend

---
+++ backend/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def main():
+    return {"message": "Portfolius API is running!"}
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}
